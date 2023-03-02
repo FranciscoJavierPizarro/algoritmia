213f3b3b0d5f748fb68cd8373e9a92f708fbb59d
FranciscoJavierPizarro <pizarrojavier000@gmail.com>
2023-03-02 16:17:39 +0100

    medir tiempos haskell

--- practica2/main_hs.hs
+++ practica2/main_hs.hs
@@ -1,10 +1,17 @@
 import BinaryTree
 import System.Environment
+import System.CPUTime
+import Control.Exception
+import Text.Printf
 -- compilar ghc main_hs.hs
 -- ejecutar ./main_hs 100000 100003
 
 main :: IO ()
 main = do
   args <- getArgs
+  start <- getCPUTime
   let resultado = simulameEsta (read (head args) :: Integer) (read (args !! 1) :: Integer) Empty
+  end <- getCPUTime
   print resultado
+  let time = fromIntegral (end - start) / (10^9) * 1000 -- seconds to milliseconds
+  printf "Time: %.3f ms\n" (time :: Double)
