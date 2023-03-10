6a7fea84ed1c05ad4a5c1a81bb101d694808b202
FranciscoJavierPizarro <821259@unizar.es>
2023-03-11 00:02:10 +0100

    pruebas hs

--- practica2/BinaryTree.hs
+++ practica2/BinaryTree.hs
@@ -1,63 +1,68 @@
 module BinaryTree where
 import Data.List
+import Data.Sequence
 import Data.Bits
+-- import Math.NumberTheory.Logarithms
+
 
 data Tree a = Branch a | Empty
 
-simulameEsta :: Integer -> Integer -> Tree Integer -> Integer
-simulameEsta 1 _ (Branch valor) = valor
-simulameEsta p n (Branch valor) = simulameEsta p' n' subarbol where
+simularConRecorridoParcial :: Integer -> Integer -> Tree Integer -> Integer
+simularConRecorridoParcial 1 _ (Branch valor) = valor
+simularConRecorridoParcial p n (Branch valor) = simularConRecorridoParcial p' n' subarbol where
     p' = p - 1
-    n' =  ((n `div` 2) :: Integer) + (if n `mod` 2 == 1 then 1 else 0) 
-    valorSiguienteNodo = if n `mod` 2 == 0 then 2*valor+1 else 2*valor where
+    valorMod = (n `mod` 2) == 1
+    nuevoValor = 2*valor
+    n' =  ((n `div` 2) :: Integer) + (if valorMod then 1 else 0) 
+    valorSiguienteNodo = if not valorMod then nuevoValor+1 else nuevoValor where
     subarbol = Branch valorSiguienteNodo
-simulameEsta p n _ = simulameEsta p' n' subarbol where
+simularConRecorridoParcial p n _ = simularConRecorridoParcial p' n' subarbol where
     p' = p - 1
-    n' = ((n `div` 2) :: Integer) + (if n `mod` 2 == 1 then 1 else 0) 
-    valorSiguienteNodo = if n `mod` 2 == 0 then 3 else 2 where
-    subarbol = Branch valorSiguienteNodo
-
-aiudaPuto :: Integer -> Integer -> Integer -> Integer
-aiudaPuto x y i 
-    | y - ((2 :: Integer) `shiftL` (fromInteger (x-4-i))) < 0   =  aiudaPuto x y (i+1)
-    | otherwise                                                 = simulamelaDirecta x ( y - (2 :: Integer) `shiftL` fromInteger (x-4-i)) + ((2 :: Integer) `shiftL` fromInteger(i))
-
-
-
-simulamelaDirecta :: Integer -> Integer -> Integer
-simulamelaDirecta x 0 = ((2 :: Integer) `shiftL` (fromInteger (x-1)))  - 1
-simulamelaDirecta x 1 = ((2 :: Integer) `shiftL` (fromInteger (x-2)))
-simulamelaDirecta x y
-    | y >= ((2 :: Integer) `shiftL` (fromInteger (x-2)))    = simulamelaDirecta x ( y `mod` ((2 :: Integer) `shiftL` (fromInteger (x-2))) )
-    -- Cuando hay más bolas de las que caben en el árbol
-    | y .&. (y-1) == 0                                      = ((2 :: Integer) `shiftL` (fromInteger (x-1))) - ((2 :: Integer) `shiftL` (fromInteger (x-2-(floor (sqrt (fromIntegral y))))))
-    -- Cuando la bola es un número cuadrado de 2
-    | (y+1) .&. y == 0                                      = simulamelaDirecta x (y+1) - ((2 :: Integer) `shiftL` (fromInteger (x-3)))
-    -- Cuando la bola más 1 es un número cuadrado de 2
-    | y > ((2 :: Integer) `shiftL` (fromInteger (x-3)))     = simulamelaDirecta x ( y - ((2 :: Integer) `shiftL` (fromInteger (x-3))) ) + 1
-    -- Cuando la bola pasa de la mitad del número de hojas del árbol
-    | y `mod` 2 == 0                                        = simulamelaDirecta x ( y-1 ) + ((2 :: Integer) `shiftL` (fromInteger (x-3)))
-    -- Cuando la bola es un número par
-    | (y-1) .&. (y-2) == 0                                  = simulamelaDirecta x 1 + ((2 :: Integer) `shiftL` (fromInteger (x-3-(fromIntegral (round (logBase 2 (fromIntegral (y-1))))))))
-    -- Cuando la bola menos 1 es un número cuadrado de 2
-    | otherwise                                             = aiudaPuto x y 0
-    -- En cualqiuier otro caso
-
-
-
-
-
-simulameEstaOptimizada :: Integer -> Integer -> Tree Integer -> Integer
-simulameEstaOptimizada 1 _ (Branch valor) = valor
-simulameEstaOptimizada p n (Branch valor) = simulameEstaOptimizada p' n' subarbol where
-    p' = p - 1
-    valorMod = (n .&. 1) == 1
-    n' =  ((n `shiftR` 1) :: Integer) + (if valorMod then 1 else 0) 
-    valorSiguienteNodo = if not valorMod then 2*valor+1 else 2*valor where
-    subarbol = Branch valorSiguienteNodo
-simulameEstaOptimizada p n _ = simulameEstaOptimizada p' n' subarbol where
-    p' = p - 1
-    valorMod = (n .&. 1) == 1
-    n' = ((n `shiftR` 1) :: Integer) + (if valorMod then 1 else 0) 
+    valorMod = (n `mod` 2) == 1
+    n' = ((n `div` 2) :: Integer) + (if valorMod then 1 else 0) 
     valorSiguienteNodo = if not valorMod then 3 else 2 where
     subarbol = Branch valorSiguienteNodo
+
+simulacionDirecta :: Integer -> Integer -> Integer
+simulacionDirecta x 0 = ((2 :: Integer) `shiftL` (fromInteger (x-1)))  - 1
+simulacionDirecta x 1 = ((2 :: Integer) `shiftL` (fromInteger (x-2)))
+simulacionDirecta x y
+    | y >= ((2 :: Integer) `shiftL` (fromInteger (x-2)))    = simulacionDirecta x ( y `mod` ((2 :: Integer) `shiftL` (fromInteger (x-2))) )
+    -- Cuando hay más bolas de las que caben en el árbol
+    | y .&. (y-1) == 0                                      = ((2 :: Integer) `shiftL` (fromInteger (x-1))) - ((2 :: Integer) `shiftL` (fromInteger (x-2-(fromIntegral (round (logBase 2 (fromIntegral y)))))))
+    -- Cuando la bola es un número cuadrado de 2
+    | (y+1) .&. y == 0                                      = simulacionDirecta x (y+1) - ((2 :: Integer) `shiftL` (fromInteger (x-3)))
+    -- Cuando la bola más 1 es un número cuadrado de 2
+    | y > ((2 :: Integer) `shiftL` (fromInteger (x-3)))     = simulacionDirecta x ( y - ((2 :: Integer) `shiftL` (fromInteger (x-3)))) + 1
+    -- Cuando la bola pasa de la mitad del número de hojas del árbol
+    | y `mod` 2 == 0                                        = simulacionDirecta x ( y-1 ) + ((2 :: Integer) `shiftL` (fromInteger (x-3)))
+    -- Cuando la bola es un número par
+    | (y-1) .&. (y-2) == 0                                  = simulacionDirecta x 1 + ((2 :: Integer) `shiftL` (fromInteger (x-3-(fromIntegral (round (logBase 2 (fromIntegral (y-1))))))))
+    -- Cuando la bola menos 1 es un número cuadrado de 2
+    | otherwise                                             = simulacionDirecta x ( y - ((2 :: Integer) `shiftL` ((fromIntegral (round (logBase 2 (fromIntegral y)))) - 2))) + ((2 :: Integer) `shiftL` (fromInteger(x - 2 - (fromIntegral (round (logBase 2 (fromIntegral y)))))))
+    -- En cualqiuier otro caso
+
+-- simulacionOptimizada :: Integer -> Integer -> Tree Integer -> Integer
+-- simulacionOptimizada 1 _ (Branch valor) = valor
+-- simulacionOptimizada p n (Branch valor) = simulacionOptimizada p' n' subarbol where
+--     p' = p - 1
+--     valorMod = (n .&. 1) == 1
+--     n' =  ((n `shiftR` 1) :: Integer) + (if valorMod then 1 else 0) 
+--     valorSiguienteNodo = if not valorMod then 2*valor+1 else 2*valor where
+--     subarbol = Branch valorSiguienteNodo
+-- simulacionOptimizada p n _ = simulacionOptimizada p' n' subarbol where
+--     p' = p - 1
+--     valorMod = (n .&. 1) == 1
+--     n' = ((n `shiftR` 1) :: Integer) + (if valorMod then 1 else 0) 
+--     valorSiguienteNodo = if not valorMod then 3 else 2 where
+--     subarbol = Branch valorSiguienteNodo
+
+
+
+-- toBinarySeq :: Integer -> Seq Bool
+-- toBinarySeq 0 = singleton False
+-- toBinarySeq n = Data.Sequence.reverse $ fromList $ map (testBit n) [0..(numBits - 1)]
+--   where numBits = finiteBitSize n
+  
+-- bitLength :: Integral a => a -> Int
+-- bitLength n = integerLog2 (fromIntegral n)
--- practica2/main_hs.hs
+++ practica2/main_hs.hs
@@ -1,19 +1,84 @@
+{-# LANGUAGE BangPatterns #-}
 import BinaryTree
 import System.Environment
 import System.CPUTime
 import Control.Exception
 import Text.Printf
--- compilar ghc main_hs.hs
--- ejecutar ./main_hs 100000 100003
+import Data.Time.Clock
+import Data.Sequence
+import Data.Bits
+-- compilar ghc main_hs.hs -threaded
+-- ejecutar time ./main_hs 10000000 10000 +RTS -s -N12
+
+-- main :: IO ()
+-- main = do
+--   let binarySeq = toBinarySeq 40
+--   print (binarySeq)
+--   args <- getArgs
+--   let n = read (head args) :: Integer
+--       m = read (args !! 1) :: Integer
+
+--   start <- getCurrentTime
+--   let !resultado = simularConRecorridoParcial n m Empty
+--   end <- getCurrentTime
+--   let diff1 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+
+--   start <- getCurrentTime
+--   let !resultado1 = simulacionDirecta n m
+--   end <- getCurrentTime
+--   let diff2 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+
+--   start <- getCurrentTime
+--   let !resultado2 = simulacionOptimizada n m Empty
+--   end <- getCurrentTime
+--   let diff3 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+
+
+--   print (resultado2 - resultado)
+--   printf "Execution time normal: \t\t%0.9f ms\n" diff1
+--   printf "Execution time direct(no tree): %0.9f ms\n" diff2
+--   printf "Execution time optimized:\t %0.9f ms\n" diff3
+
+import Control.Concurrent
 
 main :: IO ()
 main = do
   args <- getArgs
-  start <- getCPUTime
-  -- let resultado = simulamelaDirecta (read (head args) :: Integer) (read (args !! 1) :: Integer)
-  -- let resultado = simulameEsta(read (head args) :: Integer) (read (args !! 1) :: Integer) Empty
+  let n = read (head args) :: Integer
+      m = read (args !! 1) :: Integer
   
-  end <- getCPUTime
-  print resultado
-  let time = (fromIntegral (end - start) / (10^12)) * 1000 -- seconds to milliseconds
-  printf "Time: %.3f ms\n" (time :: Double)
+  -- Create an MVar for each function
+  mvars <- mapM (\_ -> newEmptyMVar) [1..3]
+
+  -- Spawn a new thread for each function and store the result in the corresponding MVar
+  -- forkIO $ do
+  --   start <- getCurrentTime
+  --   let !resultado = simulacionOptimizada n m Empty
+  --   end <- getCurrentTime
+  --   let diff1 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+  --   putMVar (mvars !! 0) (diff1, resultado)
+
+  forkIO $ do
+    start <- getCurrentTime
+    let !resultado1 = simularConRecorridoParcial n m BinaryTree.Empty
+    end <- getCurrentTime
+    let diff2 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+    putMVar (mvars !! 1) (diff2, resultado1)
+
+  forkIO $ do
+    start <- getCurrentTime
+    let !resultado2 = simulacionDirecta n m
+    end <- getCurrentTime
+    let diff3 = (realToFrac (diffUTCTime end start) :: Double) * (10^3)
+    putMVar (mvars !! 2) (diff3, resultado2)
+
+  -- Wait for all threads to finish and print the results
+  -- (diff1, resultado) <- takeMVar (mvars !! 0)
+  (diff2, resultado1) <- takeMVar (mvars !! 1)
+  (diff3, resultado2) <- takeMVar (mvars !! 2)
+
+  print (resultado1 - resultado2)
+  -- printf "Execution time optimized: \t%0.9f ms\n" diff1
+  printf "Execution time normal: \t\t%0.9f ms\n" diff2
+  printf "Execution time direct(no tree): %0.9f ms\n" diff3
+
