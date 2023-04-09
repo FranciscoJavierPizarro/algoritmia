a8f21c14bca5c3921e3839276156c46c75fac3b4
FranciscoJavierPizarro <821259@unizar.es>
2023-04-09 21:23:04 +0200

    fixes p3

--- practica3/ejecutar.sh
+++ practica3/ejecutar.sh
@@ -6,7 +6,7 @@ go build main.go
 mkdir ./tmp
 echo "Para generar los ficheros de entrada se emplea $(python3 --version)"
 #10^5 son demasiadas palabras para el lector de ficheros de golang
-for i in {1..4}; do
+for i in {1..3}; do
     n=$((10**i))
     echo "Ejecutando para ${n} palabras"
     python3 generarEntrada.py ${n}
--- practica3/generarEntrada.py
+++ practica3/generarEntrada.py
@@ -2,8 +2,8 @@ import random
 import string
 import sys
 num_words = int(sys.argv[1])
-words = set(''.join(random.choice(string.ascii_lowercase) for _ in range(5)) for _ in range(num_words))
-first_line = ''.join(random.sample(list(words), random.randint(1, num_words)))
+words = set(''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(2,5))) for _ in range(num_words))
+first_line = ''.join(random.sample(list(words), random.randint(num_words/10, num_words)))
 with open('./tmp/f' + sys.argv[1] + '.txt', 'w') as f:
     f.write(first_line + '\n')
     for word in words:
