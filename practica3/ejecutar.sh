0f62544bf309d912a1130b6071303d5442ed6c5e
FranciscoJavierPizarro <pizarrojavier000@gmail.com>
2023-04-11 15:43:21 +0200

    adaptado para ejecutar en hendrix

--- practica3/ejecutar.sh
+++ practica3/ejecutar.sh
@@ -14,18 +14,18 @@ for i in {1..3}; do
     echo "${n} $(./main ./tmp/f${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalc.txt
     echo "${n} $(./main ./tmp/f${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonfly.txt
 done
-n=2000
-echo "Ejecutando para ${n} palabras con los ficheros sin modificaciones"
-python3 generarEntrada.py ${n}
-echo "${n} $(./main ./tmp/f${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalc.txt
-echo "${n} $(./main ./tmp/f${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonfly.txt
+# n=2000
+# echo "Ejecutando para ${n} palabras con los ficheros sin modificaciones"
+# python3 generarEntrada.py ${n}
+# echo "${n} $(./main ./tmp/f${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalc.txt
+# echo "${n} $(./main ./tmp/f${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonfly.txt
 for i in {1..3}; do
     n=$((10**i))
     echo "Ejecutando para ${n} palabras con los ficheros con modificaciones"
     echo "${n} $(./main ./tmp/fMod${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalcMod.txt
     echo "${n} $(./main ./tmp/fMod${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonflyMod.txt
 done
-n=2000
-echo "Ejecutando para ${n} palabras con los ficheros con modificaciones"
-echo "${n} $(./main ./tmp/fMod${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalcMod.txt
-echo "${n} $(./main ./tmp/fMod${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonflyMod.txt
+# n=2000
+# echo "Ejecutando para ${n} palabras con los ficheros con modificaciones"
+# echo "${n} $(./main ./tmp/fMod${n}.txt 1 | cut -d ' ' -f 2)" >> ./rendimientos/precalcMod.txt
+# echo "${n} $(./main ./tmp/fMod${n}.txt 0 | cut -d ' ' -f 2)" >> ./rendimientos/pdonflyMod.txt
