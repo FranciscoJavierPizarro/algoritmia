e91d8d34a19acb01c87563c7da383b8132763d25
FranciscoJavierPizarro <pizarrojavier000@gmail.com>
2023-05-20 12:05:47 +0200

    algoritmo poda funcionando

--- practica4/main.go
+++ practica4/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"sort"
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Pedido struct {
-	Passenger int
-    StartPos int
-    EndPos   int
+	Passenger   int
+	StartPos    int
+	EndPos      int
 	TicketValue int
 }
 
 // Pedidos contiene todos los pedidos restantes
-// Capacidad disp contiene la capacidad total (capacidad * m) menos la capacidad real usada(suma del vector) 
+// Capacidad disp contiene la capacidad total (capacidad * m) menos la capacidad real usada(suma del vector)
 func estimatedMaxIncome(pedidos []Pedido, capacidadDisp int) int {
 	aux := capacidadDisp
 	for _, pedido := range pedidos {
@@ -29,17 +30,33 @@ func estimatedMaxIncome(pedidos []Pedido, capacidadDisp int) int {
 	return capacidadDisp - aux
 }
 
-func recursiveSearchDFS(pedidos []Pedido, capacidad []int, capacidadDisp int, alreadyTaken []Pedido, maxIncome int, n int, m int) (int, []Pedido){
+// Máximo global para realizar la poda
+var absolutMax int
 
-	if (len(pedidos) == 0) {
+func recursiveSearchDFS(pedidos []Pedido, capacidad []int, capacidadDisp int, alreadyTaken []Pedido, maxIncome int, n int, m int) (int, []Pedido) {
+
+	if maxIncome > absolutMax {
+		absolutMax = maxIncome
+	}
+
+	if len(pedidos) == 0 { //Hemos alcanzado una hoja del árbol
 		return maxIncome, alreadyTaken
 	}
+
 	bestIncome := 0
-	bestTaken := make([]Pedido,0)
-	//FALTA LA PODA
-	//FALTA SIMULAR LA BAJADA??
-	newCapacidad := capacidad
+	bestTaken := make([]Pedido, 0)
+
+	//PODA
+	estimatedMax := estimatedMaxIncome(pedidos, capacidadDisp) + maxIncome
+	if estimatedMax < absolutMax {
+		// fmt.Println("Se ha podado con pedidos:", pedidos, alreadyTaken, " los maximos total y estimados eran:", absolutMax, estimatedMax)
+		return maxIncome, alreadyTaken
+	}
+
+	newCapacidad := make([]int, len(capacidad))
+	copy(newCapacidad, capacidad)
 	admisible := true
+
 	for i := pedidos[0].StartPos; i < pedidos[0].EndPos; i++ {
 		if (newCapacidad[i] + pedidos[0].Passenger) <= n {
 			newCapacidad[i] += pedidos[0].Passenger
@@ -47,23 +64,33 @@ func recursiveSearchDFS(pedidos []Pedido, capacidad []int, capacidadDisp int, al
 			admisible = false
 		}
 	}
+
 	if admisible {
 		// la solución es válida añadiendo el nuevo pedido
+		// fmt.Println("sol admisible", alreadyTaken, pedidos[0])
 		newCapacidadDisp := capacidadDisp - pedidos[0].TicketValue
 		newMaxIncome := maxIncome
-		if (maxIncome < (n*m - newCapacidadDisp)) {
+		if maxIncome < (n*m - newCapacidadDisp) {
 			newMaxIncome = n*m - newCapacidadDisp
 		}
-		bestIncome, bestTaken = recursiveSearchDFS(pedidos[1:],newCapacidad,newCapacidadDisp,append(alreadyTaken, pedidos[0]), newMaxIncome,n,m)
+		bestIncome, bestTaken = recursiveSearchDFS(pedidos[1:], newCapacidad, newCapacidadDisp, append(alreadyTaken, pedidos[0]), newMaxIncome, n, m)
+	} else {
+		// fmt.Println("sol NO admisible", alreadyTaken, pedidos[0], capacidad)
 	}
-	bestIncome2, bestTaken2 := recursiveSearchDFS(pedidos[1:],capacidad,capacidadDisp,alreadyTaken,maxIncome,n,m)
-	if bestIncome > bestIncome2 {return bestIncome, bestTaken;} else {return bestIncome2, bestTaken2;}
+
+	bestIncome2, bestTaken2 := recursiveSearchDFS(pedidos[1:], capacidad, capacidadDisp, alreadyTaken, maxIncome, n, m)
+
+	if bestIncome > bestIncome2 {
+		return bestIncome, bestTaken
+	} else {
+		return bestIncome2, bestTaken2
+	}
 }
 
-func solveProblemInstance(enunciado string, pedidosEnunciado []string) {
+func solveProblemInstance(enunciado string, pedidosEnunciado []string) (int, float64) {
 	// Extraemos los datos del enunciado
-    n, _ := strconv.Atoi(strings.Split(enunciado, " ")[0])
-    m, _ := strconv.Atoi(strings.Split(enunciado, " ")[1])
+	n, _ := strconv.Atoi(strings.Split(enunciado, " ")[0])
+	m, _ := strconv.Atoi(strings.Split(enunciado, " ")[1])
 
 	// Vector que almacena el estado de la capacidad en cada punto del recorrido
 	capacidad := make([]int, m)
@@ -82,11 +109,15 @@ func solveProblemInstance(enunciado string, pedidosEnunciado []string) {
 	sort.Slice(pedidos, func(i, j int) bool {
 		return pedidos[i].TicketValue > pedidos[j].TicketValue
 	})
-	fmt.Println(pedidos)
 
-	income := estimatedMaxIncome(pedidos, n*m)
-	fmt.Println(income)
-	fmt.Println(recursiveSearchDFS(pedidos,capacidad,n*m,make([]Pedido,0),0,n,m))
+	// fmt.Println(pedidos)
+	// income := estimatedMaxIncome(pedidos, n*m)
+	// fmt.Println("max initial income following the estimation:", income)
+
+	start := time.Now()
+	bestSol, _ := recursiveSearchDFS(pedidos, capacidad, n*m, make([]Pedido, 0), 0, n, m)
+	elapsed := float64(time.Since(start).Nanoseconds()) / 1000000.0
+	return bestSol, elapsed
 }
 
 func main() {
@@ -120,8 +151,21 @@ func main() {
 		linea = scanner.Text()
 		aux += 1
 	}
-	
-	for i:=0; i < aux; i++ {
-		solveProblemInstance(enunciados[i],pedidosEnunciados[i])
+
+	f, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
 	}
+
+	defer f.Close()
+	w := bufio.NewWriter(f)
+
+	for i := 0; i < aux; i++ {
+		absolutMax = 0
+		bestSol, elapsed := solveProblemInstance(enunciados[i], pedidosEnunciados[i])
+		w.WriteString(strconv.Itoa(bestSol) + ".0 " + strconv.FormatFloat(elapsed, 'f', -1, 64) + "\n")
+	}
+
+	w.Flush()
 }
