4389a79ebeb4a68f847fec22ed8abfb3538c9a7b
Francisco Javier Pizarro <821259@unizar.es>
2023-09-25 15:14:21 +0200

    p1 avanzada generador datos

--- avanzada/practica1/generator.rb
+++ avanzada/practica1/generator.rb
@@ -1,33 +1,23 @@
-#!/usr/bin/ruby
-# Function to generate a random integer array of a given length
+# Define a method to generate a random array of given length
 def generate_random_array(length)
-    Array.new(length) { rand(1..1000000) } # Change the range as needed
+  Array.new(length) { rand(1..1000000) } # Change the range as needed
+end
+
+# Specify the file name
+file_name = "random_arrays.txt"
+
+# Array of different lengths
+array_lengths = [5, 50, 500, 5000, 10000, 100000, 1000000, 10000000, 100000000]
+
+# Open a file for writing
+File.open(file_name, "w") do |file|
+  array_lengths.each do |length|
+    # Generate a random array
+    random_array = generate_random_array(length)
+    
+    # Write the array to the file on a separate line
+    file.puts(random_array.join(' '))
   end
-  
-  # Create three random integer arrays
-  array1 = generate_random_array(5)
-  array2 = generate_random_array(50)
-  array3 = generate_random_array(500)
-  array4 = generate_random_array(5000)
-  array5 = generate_random_array(10000)
-  array6 = generate_random_array(100000)
-  array7 = generate_random_array(1000000)
-  array8 = generate_random_array(10000000)
-  array9 = generate_random_array(100000000)
-  # Open a file for writing
-  file_name = "random_arrays.txt"
-  File.open(file_name, "w") do |file|
-    # Write each array to the file on a separate line
-    file.puts(array1.join(' '))
-    file.puts(array2.join(' '))
-    file.puts(array3.join(' '))
-    file.puts(array4.join(' '))
-    file.puts(array5.join(' '))
-    file.puts(array6.join(' '))
-    file.puts(array7.join(' '))
-    file.puts(array8.join(' '))
-    file.puts(array9.join(' '))
-  end
-  
-  puts "Random arrays have been written to #{file_name}"
-  
+end
+
+puts "Random arrays have been written to #{file_name}"
