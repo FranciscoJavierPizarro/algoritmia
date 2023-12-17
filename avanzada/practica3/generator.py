bab28b716357862bee42831d527ec6b7e130cf08
FranciscoJavierPizarro <821259@unizar.es>
2023-12-17 17:43:45 +0100

    fix lib p3

--- avanzada/practica3/generator.py
+++ avanzada/practica3/generator.py
@@ -11,7 +11,7 @@
 ##############################################################################
 import random
 import networkx as nx
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def generate_graph(nodes):
     graph = nx.Graph()
